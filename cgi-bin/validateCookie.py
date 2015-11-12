cafb1ba9bfe31b00ff7ba74a5b7d3a33cf30554d
Thealexmai <Alex@dhcp-10-5-5-0.wireless.rochester.edu>
2015-11-12 00:18:02 -0500

    Attempt 2

--- cgi-bin/validateCookie.py
+++ cgi-bin/validateCookie.py
@@ -20,24 +20,24 @@ print
 
 # If no cookies
 if not stored_cookie_string:
-	data['hasCookie']=False
-	data['match']=False
+	data['hasCookie']="False"
+	data['match']="False"
 	# print json.dumps(data)
 else:
 	# load() parses the cookie string
 	cookie.load(stored_cookie_string)
-	my_emails = cookie['email']
-	for r in c.execute('select * from users where email=?;' [my_emails]):
+	my_emails = cookie['email'].value
+	for r in c.execute('select * from users where email=?;', [my_emails]):
 		checkEmail = r[0]
 		if(checkEmail == my_emails):
 			# If the email in database matches email in cookie
-			data['hasCookie']=True
-			data['match']=True
+			data['hasCookie']="True"
+			data['match']="True"
 			# print json.dumps(data)
 		else:
 			# otherwise, it has a cookie, but wrong cookie
-			data['hasCookie']=True
-			data['match']=False
+			data['hasCookie']="True"
+			data['match']="False"
 			# print json.dumps(data)
 print json.dumps(data)
 
