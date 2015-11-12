267610196aa167dd4dbe551f96be91f211775d34
Thealexmai <Alex@dhcp-10-5-6-224.wireless.rochester.edu>
2015-11-12 11:46:03 -0500

    Persistent Cookie Checking Completed!
    Finally.

--- cgi-bin/validateCookie.py
+++ cgi-bin/validateCookie.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python
-import cgitb
+import cgitb, sqlite3, Cookie, os, json
+
 cgitb.enable()
 
-import sqlite3
-conn=sqlite3.connect('accounts.db')
-c=conn.cursor()
-
-import Cookie
-import os
+conn = sqlite3.connect('accounts.db')
+c = conn.cursor()
 
 stored_cookie_string = os.environ.get('HTTP_COOKIE')
+cookie = Cookie.SimpleCookie(stored_cookie_string)
 
 print "Content-Type: text/html"
 print
 
-import json
 data = {}
 
-# If no cookies
+# If there is absolutely no cookie stored
 if not stored_cookie_string:
-	data['hasCookie']="False"
-	data['match']="False"
-	# print json.dumps(data)
+    data['hasCookie'] = False
+    data['match'] = False
+    # print json.dumps(data)
 
 else:
-	cookie = Cookie.SimpleCookie(stored_cookie_string)
+	# Grab the value from the cookie
 	my_emails = cookie["email"].value
+
 	for r in c.execute('select * from users where email=?;', [my_emails]):
-		checkEmail = r[0]
-		if(checkEmail == my_emails):
-			# If the email in database matches email in cookie
-			data['hasCookie']="True"
-			data['match']="True"
-			# print json.dumps(data)
-		else:
-			# otherwise, it has a cookie, but wrong cookie
-			data['hasCookie']="True"
-			data['match']="False"
-			# print json.dumps(data)
+	    checkEmail = r[0]
+	    if (checkEmail == my_emails):
+	        # If the email in database matches email in cookie
+	        data['hasCookie'] = True
+	        data['match'] = True
+
+	# If it has a cookie of key email, but value is not inside database
+	c.execute('select * from users where email=?;', [my_emails]);
+	results = c.fetchall()
+	if(len(results) <= 0):
+		data['hasCookie'] = True
+		data['match'] = False
 
 print json.dumps(data)
 
